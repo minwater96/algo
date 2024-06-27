aba65ac8aca60691ae79fece385d530a792b66e3
minwater96 <rlaalstn2568@naver.com>
2024-06-27 16:47:22 +0900

    수정

--- programmers/양꼬치/sol.py
+++ programmers/양꼬치/sol.py
@@ -1,4 +1,16 @@
+def solution(n, k):
+    answer = 0
+    # n : 양꼬치 몇인분 / K : 음료수
 
+    food_price = n * 12000
+    
+    if n > 10:
+        service = n // 10
+        drink_price = (k - service) * 2000
+    else:
+        drink_price = k * 2000
+
+    return food_price + drink_price
 
 print(solution(10, 3)) # => 124,000
 print(solution(64, 6)) # => 768,000
